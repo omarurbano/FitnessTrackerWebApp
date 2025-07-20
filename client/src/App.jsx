649fa33abff289b6c1a4a768c6e9c0d7404d210c
Ivan Quintero <ivanquintero@Ivans-MacBook-Air.local>
2025-07-19 22:23:53 -0700

    Added similar functions to useAuth.jsx to ensure there is a single shared state. This was needed to manage the navbar components to chnge based on user login status.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,41 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useOutletContext } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { useState, useEffect } from "react";
+import authService from "./services/authService";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const login = async (email, password) => {
+    const response = await authService.signin(email, password);
+    if (response.data.status === "SUCCESS" && response.data.validUser) {
+        const userData = response.data.data;
+        localStorage.setItem('user', JSON.stringify(userData));
+        setUser(userData);
+    }
+    return response;
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+  };
+
   return (
     <div className="w-full p-6 flex flex-col min-h-screen">
       <header>
-        <Navbar />
+        <Navbar user={user} logout={logout} />
       </header>
       <main className="flex-grow">
-        <Outlet />
+        <Outlet context={{ login }} />
       </main>
       <footer>
         <Footer/>
